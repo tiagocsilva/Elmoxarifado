4e90a376b6ee2956e9cdbd7c0c04e3bd5d23c2a8
Tiago <ltiago90@gmail.com>
2017-04-25 23:25:40 -0300

    Update site elmo!

--- Presentation/js/routes.js
+++ Presentation/js/routes.js
@@ -1,4 +1,4 @@
-var app = angular.module("elmoapp", ["ngRoute"]);
+var app = angular.module("elmoapp", ["ngRoute","ngAnimate"]);
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -7,5 +7,6 @@ app.config(function($routeProvider) {
     })
     .when("/administracao/usuarios/novo", {
         templateUrl : "administracao/usuarios/novo.html"
-    });
+    }).
+    otherwise({redirectTo: "/administracao"});
 });
