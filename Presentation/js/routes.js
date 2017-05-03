5fced6798cf8cee5d0d29963e7dfb547cbbdee0e
Tiago <ltiago90@gmail.com>
2017-05-02 23:32:39 -0300

    News Elmo

--- Presentation/js/routes.js
+++ Presentation/js/routes.js
@@ -7,6 +7,15 @@ app.config(function($routeProvider) {
     })
     .when("/administracao/usuarios/novo", {
         templateUrl : "administracao/usuarios/novo.html"
-    }).
-    otherwise({redirectTo: "/administracao"});
+    })
+    .when("/administracao/usuarios/gerenciar", {
+        templateUrl : "administracao/usuarios/gerenciar.html"
+    })
+    .when("/administracao/ferramentas/nova", {
+        templateUrl : "administracao/ferramentas/nova.html"
+    })
+    .when("/administracao/sobre", {
+        templateUrl : "administracao/sobre/sobre.html"
+    })
+    .otherwise({redirectTo: "/administracao"});
 });
